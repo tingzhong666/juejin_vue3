84af09f2108779f31cfb86db79539d29ea1ec482
tingzhong666 <tingzhong666@163.com>
2023-10-27 22:05:53 +0800

    feat: 全局功能按钮 组件 完成部分

---
+++ src/components/global_suspension.vue
@@ -0,0 +1,66 @@
+<script setup>
+</script>
+
+<template>
+  <div class="global-suspension">
+    <!-- 返回顶部 -->
+    <el-backtop :right="0" :bottom="0" class="my-backtop" />
+
+    <!-- 反馈 -->
+    <el-card class="item"><el-icon class="iconfont icon-pinglun1"></el-icon></el-card>
+
+    <!-- 菜单小按钮 -->
+    <el-popover placement="left" :width="150" trigger="hover">
+      <template #reference>
+        <el-card class="item"><el-icon>
+            <MoreFilled />
+          </el-icon></el-card>
+      </template>
+      <template #default>
+        <!-- 几个小菜单项 -->
+        <div class="menu">
+          <div class="menu-item download">下载APP</div>
+          <div class="menu-item weixin">微信公众号</div>
+          <div class="menu-item xinlang">新浪微博</div>
+        </div>
+      </template>
+    </el-popover>
+  </div>
+</template>
+
+<style scoped>
+.global-suspension {
+  position: fixed;
+  right: 30px;
+  bottom: 30px;
+}
+
+.my-backtop {
+  position: relative;
+}
+
+.item {
+  background-color: #fff;
+  font-size: 20px;
+  font-weight: 700;
+  border-radius: 50%;
+  color: var(--el-color-primary);
+  padding: 6px;
+  width: 40px;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin-top: 10px;
+  cursor: pointer;
+}
+/* 小菜单项 */
+.menu-item{
+  font-size: 14px;
+  cursor: pointer;
+  padding: 10px;
+}
+.menu-item:hover{
+  background-color: var(--el-color-info-light-9);
+}
+</style>
